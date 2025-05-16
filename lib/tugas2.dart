f7ac1db2c65b4e4264b993ccd64c3bbe0d504740
ridaprojectppkd <ridaprojectppkd@gmail.com>
2025-05-16 12:31:11 +0700

    update komen

--- lib/tugas2.dart
+++ lib/tugas2.dart
@@ -5,15 +5,18 @@ void tugas2() {
   double rataRataNilai = (uts + uas) / 2;
   bool lulus =
       rataRataNilai >= 70 && kehadiran >= 75 && (uts >= 60 || uas >= 60);
-      ///pakai and && karena ada 2 kondisi seperti if else
-      ///pakai >= menunjukan minimal
-      ///pakai bool untuk menyatakan 2 kondisi apakah nanti false atau true
+
+  ///pakai and && karena ada 2 kondisi seperti if else
+  ///pakai >= menunjukan minimal
+  ///pakai bool untuk menyatakan 2 kondisi apakah nanti false atau true
 
   print("Nilai UTS: $uts");
   print("Nilai UAS: $uas");
   print("Kehadiran : $kehadiran" + "%");
+
   ///double kehadiran >>> nilai kehadiran dalam persen
-/// print status
+  ///test github
+  /// print status
   if (lulus) {
     print("Status: lulus");
   } else {
